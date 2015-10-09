d4cf5aad1941d53516b9233f93d26e6180f92a02
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-10-09 00:48:08 -0400

    switch to read the docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -110,10 +110,6 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'alabaster'
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
