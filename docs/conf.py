37b97a26be105659b578ab013cba31db3ede593d
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-10-09 01:48:17 -0400

    fix author in pdf

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Watson Developer Cloud Python SDK'
 copyright = u'2015, IBM Corp'
-author = u'Jeff Stylos @ IBM Corp'
+author = u'IBM Corp'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
