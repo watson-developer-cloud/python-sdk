6bccdbdd5b44b6a11d603dd6f062e7b72b60249a
Jeffrey Stylos <jsstylos@gmail.com>
2016-05-31 15:28:32 -0400

    updating .env, classifier id

--- examples/natural_language_classifier_v1.py
+++ examples/natural_language_classifier_v1.py
@@ -12,13 +12,13 @@ print(json.dumps(classifiers, indent=2))
 
 # create a classifier
 # with open('../resources/weather_data_train.csv', 'rb') as training_data:
-#     print(json.dumps(natural_language_classifier.create(training_data=training_data, name='weather2'), indent=2))
+#     print(json.dumps(natural_language_classifier.create(training_data=training_data, name='weather'), indent=2))
 
 # replace 4f1704ex55-nlc-2432" with your classifier id
-status = natural_language_classifier.status('f1704ex55-nlc-2432')
+status = natural_language_classifier.status('3a84d1x62-nlc-18096')
 print(json.dumps(status, indent=2))
 #
-classes = natural_language_classifier.classify('f1704ex55-nlc-2432', 'How hot will it be tomorrow?')
+classes = natural_language_classifier.classify('3a84d1x62-nlc-18096', 'How hot will it be tomorrow?')
 print(json.dumps(classes, indent=2))
 
 # example of raising a WatsonException
