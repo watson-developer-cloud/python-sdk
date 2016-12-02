7cef798ced62fd8ed8c5c588ee7e36d84fde9105
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2016-12-02 10:07:42 -0500

    added example

---
+++ examples/discovery_v1.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import watson_developer_cloud
+import os,json
+
+discovery = watson_developer_cloud.DiscoveryV1('2016-11-07', username=os.environ.get('DISCOVERY_USERNAME'),
+                                               password=os.environ.get('DISCOVERY_PASSWORD'))
+
+environments = discovery.get_environments()
+news_environments = [x for x in environments['environments'] if x['name'] == 'Watson News Environment']
+news_environment_id = news_environments[0]['environment_id']
+print(news_environment_id)
+
+collections = discovery.get_collections(news_environment_id)
+news_collections = [x for x in collections['collections']]
+print(collections)
