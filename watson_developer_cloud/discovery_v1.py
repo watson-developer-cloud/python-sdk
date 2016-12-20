57cf681142221977112c18ff542dace615d924ef
Joshua Smith <kognate@gmail.com>
2016-12-20 11:49:15 -0500

    adds the discovery v1 and examples (#135)
    * adds the discovery v1 and examples
    
    * updated to fix pylint reported problems
    
    * couple more pylint issues
    
    * print 2.7 vs 3 problems with pylint
    
    * remove unused var
    
    * update examples to be runnable on travis

--- watson_developer_cloud/discovery_v1.py
+++ watson_developer_cloud/discovery_v1.py
@@ -16,6 +16,7 @@ The v1 Discovery Service
 (http://www.ibm.com/watson/developercloud/doc/discovery/)
 """
 import json
+import mimetypes
 
 from .watson_developer_cloud_service import WatsonDeveloperCloudService
 
@@ -80,12 +81,14 @@ class DiscoveryV1(WatsonDeveloperCloudService):
         if size not in range(1, 4):
             raise ValueError("Size can be 1, 2, or 3")
 
-        body = json.dumps(
-            {"name": name, "description": description, "size": size})
+        body = json.dumps({"name": name,
+                           "description": description,
+                           "size": size})
         return self.request(method='POST',
                             url='/v1/environments',
-                            params={"version": self.version,
-                                    "body": body},
+                            params={"version": self.version},
+                            data=body,
+                            headers={'content-type': 'application/json'},
                             accept_json=True)
 
     def update_environment(self, environment_id, name="", description=""):
@@ -100,8 +103,9 @@ class DiscoveryV1(WatsonDeveloperCloudService):
         body = json.dumps({"name": name, "description": description})
         return self.request(method='PUT',
                             url='/v1/environments/{0}'.format(environment_id),
-                            params={"version": self.version,
-                                    "body": body},
+                            params={"version": self.version},
+                            data=body,
+                            headers={'content-type': 'application/json'},
                             accept_json=True)
 
     def delete_environment(self, environment_id):
@@ -112,17 +116,50 @@ class DiscoveryV1(WatsonDeveloperCloudService):
         """
         url_string = '/v1/environments/{0}'.format(environment_id)
         return self.request(method='DELETE', url=url_string,
-                            params={"version": self.version}, accept_json=True)
+                            params={"version": self.version},
+                            accept_json=True)
 
-    def get_collections(self, environment_id):
+    def list_configurations(self, environment_id):
+
+        format_string = '/v1/environments/{0}/configurations'
+        url_string = format_string.format(environment_id)
+        return self.request(method='GET', url=url_string,
+                            params={'version': self.version},
+                            accept_json=True)
+
+    def get_default_configuration_id(self, environment_id):
+        all_configs = self.list_configurations(environment_id=environment_id)
+        try:
+            configs = [x['configuration_id']
+                       for x in all_configs['configurations']
+                       if x['name'] == 'Default Configuration']
+            if len(configs) > 0:
+                return configs[0]
+            else:
+                return None
+        except KeyError:
+            pass # this isn't a problem and supress isn't in 2.7
+        return None
+
+    def get_configuration(self, environment_id, configuration_id):
+
+        format_string = '/v1/environments/{0}/configurations/{1}'
+        url_string = format_string.format(environment_id, configuration_id)
+        return self.request(method='GET', url=url_string,
+                            params={'version': self.version},
+                            accept_json=True)
+
+    def list_collections(self, environment_id):
         """
         Retrieves information about the collections within a given environment
         :param environment_id: this is the guid of a valid environment
         :return: json results of the collections in an environment
         """
-        url_string = '/v1/environments/{0}/collections'.format(environment_id)
+        url_string = '/v1/environments/{0}/collections'.format(
+            environment_id)
         return self.request(method='GET', url=url_string,
-                            params={"version": self.version}, accept_json=True)
+                            params={"version": self.version},
+                            accept_json=True)
 
     def get_collection(self, environment_id, collection_id):
         """
@@ -135,6 +172,109 @@ class DiscoveryV1(WatsonDeveloperCloudService):
                             url='/v1/environments/{0}/collections/{1}'.format(
                                 environment_id, collection_id),
                             params={"version": latest_version},
+                            accept_json=True)
+
+    def create_collection(self,
+                          environment_id,
+                          name,
+                          description="",
+                          configuration_id=None):
+
+        if configuration_id is None:
+            default_config = self.get_default_configuration_id(
+                environment_id=environment_id)
+            configuration_id = default_config
+
+        data_dict = {'configuration_id': configuration_id,
+                     'name': name,
+                     'description': description}
+        url_string = '/v1/environments/{0}/collections'.format(
+            environment_id)
+        return self.request(method='POST',
+                            url=url_string,
+                            data=data_dict,
+                            params={'version': latest_version},
+                            accept_json=True)
+
+    def list_collection_fields(self, environment_id, collection_id):
+        url_string = '/v1/environments/{0}/collections/{1}/fields'.format(
+            environment_id, collection_id)
+
+        return self.request(method='GET',
+                            url=url_string,
+                            params={'version': latest_version},
+                            accept_json=True)
+
+    def delete_collection(self, environment_id, collection_id):
+
+        url_string = '/v1/environments/{0}/collections/{1}'.format(
+            environment_id, collection_id)
+
+        return self.request(method='DELETE',
+                            url=url_string,
+                            params={'version': latest_version},
+                            accept_json=True)
+
+    def add_document(self, environment_id, collection_id, fileinfo, metadata=None):
+        url_string = '/v1/environments/{0}/collections/{1}/documents'.format(
+            environment_id, collection_id)
+
+        params = {'version': latest_version}
+
+        if metadata is None:
+            metadata = {}
+
+        mime_type = mimetypes.guess_type(
+            fileinfo.name)[0] or 'application/octet-stream'
+
+        return self.request(method='POST',
+                            url=url_string,
+                            params=params,
+                            data=metadata,
+                            files={'file': (fileinfo.name, fileinfo, mime_type),
+                                   'metadata': (None, json.dumps(metadata), 'application/json')},
+                            accept_json=True)
+
+    def test_document(self, environment_id, fileinfo, configuration_id=None, metadata=None):
+        url_string = '/v1/environments/{0}/preview'.format(
+            environment_id)
+
+        params = {'version': latest_version}
+
+        if metadata is None:
+            metadata = {}
+
+        if configuration_id:
+            params['configuration_id'] = configuration_id
+        else:
+            params['configuration_id'] = self.get_default_configuration_id(
+                environment_id=environment_id)
+
+        mime_type = mimetypes.guess_type(
+            fileinfo.name)[0] or 'application/octet-stream'
+
+        return self.request(method='POST',
+                            url=url_string,
+                            params=params,
+                            data=metadata,
+                            files={'file': (fileinfo.name,
+                                            fileinfo,
+                                            mime_type),
+                                   'metadata': (None,
+                                                json.dumps(metadata),
+                                                'application/json')},
+                            accept_json=True)
+
+    def delete_document(self, environment_id, collection_id, document_id):
+        base_url = '/v1/environments/{0}/collections/{1}/documents/{2}'
+        url_string = base_url.format(
+            environment_id, collection_id, document_id)
+
+        params = {'version': latest_version}
+
+        return self.request(method='DELETE',
+                            url=url_string,
+                            params=params,
                             accept_json=True)
 
     def query(self, environment_id, collection_id, query_options):
