4b459a367c67b40561b170b86c2df8882880d2be
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-12-20 23:55:10 -0500

    Exclude tests if there are no credentials in VCAP_SERVICES

--- test/test_examples.py
+++ test/test_examples.py
@@ -3,11 +3,13 @@
 import os
 
 import pytest
+import json as json_import
+
 from os.path import join, dirname
 from glob import glob
 
 # tests to exclude
-excludes = ['authorization_v1.py', 'alchemy_data_news_v1.py', 'alchemy_language_v1.py']
+excludes = ['authorization_v1.py', 'alchemy_data_news_v1.py', 'alchemy_language_v1.py', 'discovery_v1.ipynb', '__init__.py']
 
 # examples path. /examples
 examples_path = join(dirname(__file__), '../', 'examples', '*.py')
@@ -27,15 +29,23 @@ except:
 
 @pytest.mark.skipif(os.getenv('VCAP_SERVICES') is None, reason='requires VCAP_SERVICES')
 def test_examples():
+    vcapServices = json_import.loads(os.getenv('VCAP_SERVICES'))
     examples = glob(examples_path)
     for example in examples:
         name = example.split('/')[-1]
+
         # exclude some tests cases like authorization
         if name in excludes:
+            continue
+
+        # exclude tests if there are no credentials for that service
+        serviceName = name[:-6] if not name.startswith('visual_recognition') else 'watson_vision_combined'
+
+        if serviceName not in vcapServices:
+            print('%s does not have credentials in VCAP_SERVICES', serviceName)
             continue
 
         try:
             exec(open(example).read(), globals())
         except Exception as e:
             assert False, 'example in file ' + name + ' failed with error: ' + str(e)
-
