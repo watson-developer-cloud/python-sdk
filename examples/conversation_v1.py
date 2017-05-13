9d496d443358dd8ad82b792594ff4219e7a42dda
Glenn R. Fisher <glennrfisher@gmail.com>
2017-05-13 16:38:34 -0500

    Update example version date

--- examples/conversation_v1.py
+++ examples/conversation_v1.py
@@ -8,7 +8,7 @@ from watson_developer_cloud import ConversationV1
 conversation = ConversationV1(
     username='YOUR SERVICE USERNAME',
     password='YOUR SERVICE PASSWORD',
-    version='2016-09-20')
+    version='2017-04-21')
 
 # replace with your own workspace_id
 workspace_id = '0a0c06c1-8e31-4655-9067-58fcac5134fc'
