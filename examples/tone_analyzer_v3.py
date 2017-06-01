f799842b257b585656f74a72b0b11cc302f10dec
Jeff Kaminski <jpkamins@us.ibm.com>
2017-06-01 13:15:59 -0400

    Tone Analyzer: Add support for JSON and HTML input to tone() method

--- examples/tone_analyzer_v3.py
+++ examples/tone_analyzer_v3.py
@@ -1,13 +1,45 @@
 import json
+import os
+from os.path import join, dirname
 from watson_developer_cloud import ToneAnalyzerV3
-
 
 tone_analyzer = ToneAnalyzerV3(
     username='YOUR SERVICE USERNAME',
     password='YOUR SERVICE PASSWORD',
-    version='2016-02-11')
+    version='2016-05-19')
 
-print(json.dumps(tone_analyzer.tone(text='I am very happy'), indent=2))
-
+print("\ntone_chat() example 1:\n")
 utterances = [{'text': 'I am very happy', 'user': 'glenn'}]
 print(json.dumps(tone_analyzer.tone_chat(utterances), indent=2))
+
+print("\ntone() example 1:\n")
+print(json.dumps(tone_analyzer.tone(text='I am very happy. It is a good day.'),
+                 indent=2))
+
+print("\ntone() example 2:\n")
+with open(join(dirname(__file__),
+               '../resources/tone-example.json')) as tone_json:
+    tone = tone_analyzer.tone(json.load(tone_json)['text'],
+                              tones='emotion')
+print(json.dumps(tone, indent=2))
+
+print("\ntone() example 3:\n")
+with open(join(dirname(__file__),
+               '../resources/tone-example.json')) as tone_json:
+    tone = tone_analyzer.tone(json.load(tone_json)['text'],
+                              content_type='text/plain', tones='emotion')
+print(json.dumps(tone, indent=2))
+
+print("\ntone() example 4:\n")
+with open(join(dirname(__file__),
+               '../resources/tone-example.json')) as tone_json:
+    tone = tone_analyzer.tone(json.load(tone_json),
+                              content_type='application/json', tones='emotion')
+print(json.dumps(tone, indent=2))
+
+print("\ntone() example 5:\n")
+with open(join(dirname(__file__),
+               '../resources/tone-example-html.json')) as tone_json:
+    tone = tone_analyzer.tone(json.load(tone_json)['text'],
+                              content_type='text/html', tones='emotion')
+print(json.dumps(tone, indent=2))
