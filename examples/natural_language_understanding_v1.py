9bb0c518a4ec87434b4d330d09539f2c8646355d
00111000 <medvegalapa@gmail.com>
2017-07-14 14:24:36 -0600

    Corrected feature import statement

--- examples/natural_language_understanding_v1.py
+++ examples/natural_language_understanding_v1.py
@@ -1,7 +1,7 @@
 import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 import watson_developer_cloud.natural_language_understanding.features.v1 as \
-    features
+    Features
 
 
 natural_language_understanding = NaturalLanguageUnderstandingV1(
