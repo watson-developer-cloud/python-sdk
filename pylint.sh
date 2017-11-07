aeaf6663e4d9aee2c93977ba676c5444152d0c03
Erika Dsouza <erika27desouza@gmail.com>
2017-11-07 17:27:55 -0500

    :rainbow: Add pylint test

--- pylint.sh
+++ pylint.sh
@@ -5,4 +5,5 @@ PYTHON_VERSION=$(python -c 'import sys; print(".".join(map(str, sys.version_info
 echo "Python version: $PYTHON_VERSION"
 if [ $PYTHON_VERSION = '2.7' ]; then
   pylint watson_developer_cloud
+  pylint test
 fi
