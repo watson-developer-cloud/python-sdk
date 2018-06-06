1bc475910d9ef7d742bc43ff84577f4de0eb68dd
Justinas V. Daugmaudis <justinas@lt.ibm.com>
2018-06-06 15:25:07 +0300

    Update unit tests to ensure that the returned values are JSON dicts
    Automatic code quality checks

--- test/unit/test_assistant_v1.py
+++ test/unit/test_assistant_v1.py
@@ -105,7 +105,6 @@ def test_delete_counterexample():
         workspace_id='boguswid', text='I want financial advice today')
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith(url)
-    assert counterexample is None
 
 
 @responses.activate
@@ -241,7 +240,7 @@ def test_create_entity():
 def test_delete_entity():
     endpoint = '/v1/workspaces/{0}/entities/{1}'.format('boguswid', 'pizza_toppings')
     url = '{0}{1}'.format(base_url, endpoint)
-    response = ""
+    response = {}
     responses.add(
         responses.DELETE,
         url,
@@ -253,7 +252,6 @@ def test_delete_entity():
     entity = service.delete_entity(workspace_id='boguswid', entity='pizza_toppings')
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith(url)
-    assert entity is None
 
 
 @responses.activate
@@ -414,7 +412,6 @@ def test_delete_example():
         text='Gimme a pizza with pepperoni')
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith(url)
-    assert example is None
 
 
 @responses.activate
@@ -567,7 +564,6 @@ def test_delete_intent():
         workspace_id='boguswid', intent='pizza_order')
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith(url)
-    assert intent is None
 
 
 @responses.activate
@@ -998,7 +994,7 @@ def test_delete_synonym():
     endpoint = '/v1/workspaces/{0}/entities/{1}/values/{2}/synonyms/{3}'.format(
         'boguswid', 'aeiou', 'vowel', 'a')
     url = '{0}{1}'.format(base_url, endpoint)
-    response = ""
+    response = {}
     responses.add(
         responses.DELETE,
         url,
@@ -1011,7 +1007,6 @@ def test_delete_synonym():
         workspace_id='boguswid', entity='aeiou', value='vowel', synonym='a')
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith(url)
-    assert synonym is None
 
 
 @responses.activate
@@ -1153,7 +1148,7 @@ def test_delete_value():
     endpoint = '/v1/workspaces/{0}/entities/{1}/values/{2}'.format(
         'boguswid', 'grilling', 'bbq')
     url = '{0}{1}'.format(base_url, endpoint)
-    response = ""
+    response = {}
     responses.add(
         responses.DELETE,
         url,
@@ -1166,7 +1161,6 @@ def test_delete_value():
         workspace_id='boguswid', entity='grilling', value='bbq')
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith(url)
-    assert value is None
 
 
 @responses.activate
@@ -1330,7 +1324,6 @@ def test_delete_workspace():
     workspace = service.delete_workspace(workspace_id='boguswid')
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url.startswith(url)
-    assert workspace is None
 
 
 @responses.activate
