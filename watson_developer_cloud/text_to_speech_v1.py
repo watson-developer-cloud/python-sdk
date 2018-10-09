7889765d267aa2f9b252e1f4d54664f6b02da4c0
Erika-Dsouza <erika.dsouza@ibm.com>
2018-10-09 15:09:47 -0400

    chore(TSS): Regenerate text to speech

--- watson_developer_cloud/text_to_speech_v1.py
+++ watson_developer_cloud/text_to_speech_v1.py
@@ -15,51 +15,20 @@
 # limitations under the License.
 """
 ### Service Overview
-The IBM&reg; Text to Speech service provides an API that uses IBM's speech-synthesis
+The IBM&reg; Text to Speech service provides APIs that use IBM's speech-synthesis
 capabilities to synthesize text into natural-sounding speech in a variety of languages,
 dialects, and voices. The service supports at least one male or female voice, sometimes
-both, for each language. The audio is streamed back to the client with minimal delay. For
-more information about the service, see the [IBM&reg; Cloud
-documentation](https://console.bluemix.net/docs/services/text-to-speech/index.html).
-### API usage guidelines
-* **Audio formats:** The service can produce audio in many formats (MIME types). See
-[Specifying an audio
-format](https://console.bluemix.net/docs/services/text-to-speech/http.html#format).
-* **SSML:** Many methods refer to the Speech Synthesis Markup Language (SSML). SSML is an
+both, for each language. The audio is streamed back to the client with minimal delay.
+For speech synthesis, the service supports a synchronous HTTP Representational State
+Transfer (REST) interface. It also supports a WebSocket interface that provides both plain
+text and SSML input, including the SSML &lt;mark&gt; element and word timings. SSML is an
 XML-based markup language that provides text annotation for speech-synthesis applications.
-See [Using SSML](https://console.bluemix.net/docs/services/text-to-speech/SSML.html) and
-[Using IBM SPR](https://console.bluemix.net/docs/services/text-to-speech/SPRs.html).
-* **Word translations:** Many customization methods accept sounds-like or phonetic
-translations for words. Phonetic translations are based on the SSML phoneme format for
-representing a word. You can specify them in standard International Phonetic Alphabet
-(IPA) representation
-  &lt;phoneme alphabet="ipa" ph="t&#601;m&#712;&#593;to"&gt;&lt;/phoneme&gt;
-  or in the proprietary IBM Symbolic Phonetic Representation (SPR)
-  &lt;phoneme alphabet="ibm" ph="1gAstroEntxrYFXs"&gt;&lt;/phoneme&gt;
-  See [Understanding
-customization](https://console.bluemix.net/docs/services/text-to-speech/custom-intro.html).
-* **WebSocket interface:** The service also offers a WebSocket interface for speech
-synthesis. The WebSocket interface supports both plain text and SSML input, including the
-SSML &lt;mark&gt; element and word timings. See [The WebSocket
-interface](https://console.bluemix.net/docs/services/text-to-speech/websockets.html).
-* **Customization IDs:** Many methods accept a customization ID, which is a Globally
-Unique Identifier (GUID). Customization IDs are hexadecimal strings that have the format
-`xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx`.
-* **`X-Watson-Learning-Opt-Out`:** By default, all Watson services log requests and their
-results. Logging is done only to improve the services for future users. The logged data is
-not shared or made public. To prevent IBM from accessing your data for general service
-improvements, set the `X-Watson-Learning-Opt-Out` request header to `true` for all
-requests. You must set the header on each request that you do not want IBM to access for
-general service improvements.
-  Methods of the customization interface do not log words and translations that you use to
-build custom voice models. Your training data is never used to improve the service's base
-models. However, the service does log such data when a custom model is used with a
-synthesize request. You must set the `X-Watson-Learning-Opt-Out` request header to `true`
-to prevent IBM from accessing the data to improve the service.
-* **`X-Watson-Metadata`:** This header allows you to associate a customer ID with data
-that is passed with a request. If necessary, you can use the **Delete labeled data**
-method to delete the data for a customer ID. See [Information
-security](https://console.bluemix.net/docs/services/text-to-speech/information-security.html).
+The service also offers a customization interface. You can use the interface to define
+sounds-like or phonetic translations for words. A sounds-like translation consists of one
+or more words that, when combined, sound like the word. A phonetic translation is based on
+the SSML phoneme format for representing a word. You can specify a phonetic translation in
+standard International Phonetic Alphabet (IPA) representation or in the proprietary IBM
+Symbolic Phonetic Representation (SPR).
 """
 
 from __future__ import absolute_import
@@ -141,6 +110,8 @@ class TextToSpeechV1(WatsonService):
         language, gender, and other details about the voice. Specify a customization ID to
         obtain information for that custom voice model of the specified voice. To list
         information about all available voices, use the **List voices** method.
+        **See also:** [Specifying a
+        voice](https://console.bluemix.net/docs/services/text-to-speech/http.html#voices).
 
         :param str voice: The voice for which information is to be returned.
         :param str customization_id: The customization ID (GUID) of a custom voice model
@@ -174,6 +145,8 @@ class TextToSpeechV1(WatsonService):
         Lists all voices available for use with the service. The information includes the
         name, language, gender, and other details about the voice. To see information
         about a specific voice, use the **Get a voice** method.
+        **See also:** [Specifying a
+        voice](https://console.bluemix.net/docs/services/text-to-speech/http.html#voices).
 
         :param dict headers: A `dict` containing the request headers
         :return: A `DetailedResponse` containing the result, headers and HTTP status code.
@@ -203,16 +176,15 @@ class TextToSpeechV1(WatsonService):
         Synthesizes text to spoken audio, returning the synthesized audio stream as an
         array of bytes. You can pass a maximum of 5 KB of text.  Use the `Accept` header
         or the `accept` query parameter to specify the requested format (MIME type) of the
-        response audio. By default, the service uses `audio/ogg;codecs=opus`. For detailed
-        information about the supported audio formats and sampling rates, see [Specifying
-        an audio
-        format](https://console.bluemix.net/docs/services/text-to-speech/http.html#format).
+        response audio. By default, the service uses `audio/ogg;codecs=opus`.
         If a request includes invalid query parameters, the service returns a `Warnings`
         response header that provides messages about the invalid parameters. The warning
         includes a descriptive message and a list of invalid argument strings. For
         example, a message such as `\"Unknown arguments:\"` or `\"Unknown url query
         arguments:\"` followed by a list of the form `\"invalid_arg_1, invalid_arg_2.\"`
         The request succeeds despite the warnings.
+        **See also:** [Synthesizing text to
+        audio](https://console.bluemix.net/docs/services/text-to-speech/http.html#synthesize).
 
         :param str text: The text to synthesize.
         :param str accept: The requested audio format (MIME type) of the audio. You can
@@ -268,6 +240,8 @@ class TextToSpeechV1(WatsonService):
         specific voice to see the default translation for the language of that voice or
         for a specific custom voice model to see the translation for that voice model.
         **Note:** This method is currently a beta release.
+        **See also:** [Querying a word from a
+        language](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuWordsQueryLanguage).
 
         :param str text: The word for which the pronunciation is requested.
         :param str voice: A voice that specifies the language in which the pronunciation
@@ -323,6 +297,8 @@ class TextToSpeechV1(WatsonService):
         model. The model is owned by the instance of the service whose credentials are
         used to create it.
         **Note:** This method is currently a beta release.
+        **See also:** [Creating a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-models.html#cuModelsCreate).
 
         :param str name: The name of the new custom voice model.
         :param str language: The language of the new custom voice model. Omit the
@@ -355,6 +331,8 @@ class TextToSpeechV1(WatsonService):
         Deletes the specified custom voice model. You must use credentials for the
         instance of the service that owns a model to delete it.
         **Note:** This method is currently a beta release.
+        **See also:** [Deleting a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-models.html#cuModelsDelete).
 
         :param str customization_id: The customization ID (GUID) of the custom voice
         model. You must make the request with service credentials created for the instance
@@ -383,6 +361,8 @@ class TextToSpeechV1(WatsonService):
         and their translations as defined in the model. To see just the metadata for a
         voice model, use the **List custom models** method.
         **Note:** This method is currently a beta release.
+        **See also:** [Querying a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-models.html#cuModelsQuery).
 
         :param str customization_id: The customization ID (GUID) of the custom voice
         model. You must make the request with service credentials created for the instance
@@ -413,6 +393,8 @@ class TextToSpeechV1(WatsonService):
         credentials for the instance of the service that owns a model to list information
         about it.
         **Note:** This method is currently a beta release.
+        **See also:** [Querying all custom
+        models](https://console.bluemix.net/docs/services/text-to-speech/custom-models.html#cuModelsQueryAll).
 
         :param str language: The language for which custom voice models that are owned by
         the requesting service credentials are to be returned. Omit the parameter to see
@@ -449,7 +431,24 @@ class TextToSpeechV1(WatsonService):
         already exists in a custom model overwrites the word's existing translation. A
         custom model can contain no more than 20,000 entries. You must use credentials for
         the instance of the service that owns a model to update it.
+        You can define sounds-like or phonetic translations for words. A sounds-like
+        translation consists of one or more words that, when combined, sound like the
+        word. Phonetic translations are based on the SSML phoneme format for representing
+        a word. You can specify them in standard International Phonetic Alphabet (IPA)
+        representation
+          <code>&lt;phoneme alphabet=\"ipa\"
+        ph=\"t&#601;m&#712;&#593;to\"&gt;&lt;/phoneme&gt;</code>
+          or in the proprietary IBM Symbolic Phonetic Representation (SPR)
+          <code>&lt;phoneme alphabet=\"ibm\"
+        ph=\"1gAstroEntxrYFXs\"&gt;&lt;/phoneme&gt;</code>
         **Note:** This method is currently a beta release.
+        **See also:**
+        * [Updating a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-models.html#cuModelsUpdate)
+        * [Adding words to a Japanese custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuJapaneseAdd)
+        * [Understanding
+        customization](https://console.bluemix.net/docs/services/text-to-speech/custom-intro.html).
 
         :param str customization_id: The customization ID (GUID) of the custom voice
         model. You must make the request with service credentials created for the instance
@@ -499,7 +498,24 @@ class TextToSpeechV1(WatsonService):
         word's existing translation. A custom model can contain no more than 20,000
         entries. You must use credentials for the instance of the service that owns a
         model to add a word to it.
+        You can define sounds-like or phonetic translations for words. A sounds-like
+        translation consists of one or more words that, when combined, sound like the
+        word. Phonetic translations are based on the SSML phoneme format for representing
+        a word. You can specify them in standard International Phonetic Alphabet (IPA)
+        representation
+          <code>&lt;phoneme alphabet=\"ipa\"
+        ph=\"t&#601;m&#712;&#593;to\"&gt;&lt;/phoneme&gt;</code>
+          or in the proprietary IBM Symbolic Phonetic Representation (SPR)
+          <code>&lt;phoneme alphabet=\"ibm\"
+        ph=\"1gAstroEntxrYFXs\"&gt;&lt;/phoneme&gt;</code>
         **Note:** This method is currently a beta release.
+        **See also:**
+        * [Adding a single word to a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuWordAdd)
+        * [Adding words to a Japanese custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuJapaneseAdd)
+        * [Understanding
+        customization](https://console.bluemix.net/docs/services/text-to-speech/custom-intro.html).
 
         :param str customization_id: The customization ID (GUID) of the custom voice
         model. You must make the request with service credentials created for the instance
@@ -545,7 +561,24 @@ class TextToSpeechV1(WatsonService):
         overwrites the word's existing translation. A custom model can contain no more
         than 20,000 entries. You must use credentials for the instance of the service that
         owns a model to add words to it.
+        You can define sounds-like or phonetic translations for words. A sounds-like
+        translation consists of one or more words that, when combined, sound like the
+        word. Phonetic translations are based on the SSML phoneme format for representing
+        a word. You can specify them in standard International Phonetic Alphabet (IPA)
+        representation
+          <code>&lt;phoneme alphabet=\"ipa\"
+        ph=\"t&#601;m&#712;&#593;to\"&gt;&lt;/phoneme&gt;</code>
+          or in the proprietary IBM Symbolic Phonetic Representation (SPR)
+          <code>&lt;phoneme alphabet=\"ibm\"
+        ph=\"1gAstroEntxrYFXs\"&gt;&lt;/phoneme&gt;</code>
         **Note:** This method is currently a beta release.
+        **See also:**
+        * [Adding multiple words to a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuWordsAdd)
+        * [Adding words to a Japanese custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuJapaneseAdd)
+        * [Understanding
+        customization](https://console.bluemix.net/docs/services/text-to-speech/custom-intro.html).
 
         :param str customization_id: The customization ID (GUID) of the custom voice
         model. You must make the request with service credentials created for the instance
@@ -587,6 +620,8 @@ class TextToSpeechV1(WatsonService):
         Deletes a single word from the specified custom voice model. You must use
         credentials for the instance of the service that owns a model to delete its words.
         **Note:** This method is currently a beta release.
+        **See also:** [Deleting a word from a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuWordDelete).
 
         :param str customization_id: The customization ID (GUID) of the custom voice
         model. You must make the request with service credentials created for the instance
@@ -617,6 +652,8 @@ class TextToSpeechV1(WatsonService):
         shows the translation as it is defined in the model. You must use credentials for
         the instance of the service that owns a model to list its words.
         **Note:** This method is currently a beta release.
+        **See also:** [Querying a single word from a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuWordQueryModel).
 
         :param str customization_id: The customization ID (GUID) of the custom voice
         model. You must make the request with service credentials created for the instance
@@ -648,6 +685,8 @@ class TextToSpeechV1(WatsonService):
         must use credentials for the instance of the service that owns a model to list its
         words.
         **Note:** This method is currently a beta release.
+        **See also:** [Querying all words from a custom
+        model](https://console.bluemix.net/docs/services/text-to-speech/custom-entries.html#cuWordsQueryModel).
 
         :param str customization_id: The customization ID (GUID) of the custom voice
         model. You must make the request with service credentials created for the instance
@@ -681,8 +720,8 @@ class TextToSpeechV1(WatsonService):
         customer ID. You must issue the request with credentials for the same instance of
         the service that was used to associate the customer ID with the data.
         You associate a customer ID with data by passing the `X-Watson-Metadata` header
-        with a request that passes the data. For more information about customer IDs and
-        about using this method, see [Information
+        with a request that passes the data.
+        **See also:** [Information
         security](https://console.bluemix.net/docs/services/text-to-speech/information-security.html).
 
         :param str customer_id: The customer ID for which all data is to be deleted.
