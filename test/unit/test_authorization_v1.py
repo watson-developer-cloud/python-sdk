332db3506742e401ca6e0f7d2b180045cf377c7c
Erika-Dsouza <erika.dsouza@ibm.com>
2018-10-15 14:24:17 -0400

    fix(Authorization): Fix authorization to get from result

---
+++ test/unit/test_authorization_v1.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+import responses
+import watson_developer_cloud
+
+
+@responses.activate
+def test_request_token():
+    url = 'https://stream.watsonplatform.net/authorization/api/v1/token?url=https://stream.watsonplatform.net/speech-to-text/api'
+    responses.add(responses.GET,
+                  url=url,
+                  body=b'mocked token',
+                  status=200)
+    authorization = watson_developer_cloud.AuthorizationV1(username='xxx',
+                                                           password='yyy')
+    authorization.get_token(url=watson_developer_cloud.SpeechToTextV1.default_url)
+    assert responses.calls[0].request.url == url
+    assert responses.calls[0].response.content == 'mocked token'
