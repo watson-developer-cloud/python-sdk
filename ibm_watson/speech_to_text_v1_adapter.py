4bd210197d92d7bc82a809093647b82da08f24a5
ehdsouza <erika27desouza@gmail.com>
2020-01-16 13:49:07 -0500

    chore(copyright): correct copyright year

--- ibm_watson/speech_to_text_v1_adapter.py
+++ ibm_watson/speech_to_text_v1_adapter.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# Copyright 2018 IBM All Rights Reserved.
+# (C) Copyright IBM Corp. 2018, 2020.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
