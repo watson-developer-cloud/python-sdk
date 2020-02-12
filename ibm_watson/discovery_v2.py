d105198b5e42f279f1e20dd7b1df23696f00e2c0
ehdsouza <erika27desouza@gmail.com>
2020-02-11 19:21:54 -0500

    chore(discovery): regenerate discovery

--- ibm_watson/discovery_v2.py
+++ ibm_watson/discovery_v2.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# (C) Copyright IBM Corp. 2019, 2020.
+# (C) Copyright IBM Corp. 2020.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,8 +27,9 @@ from .common import get_sdk_headers
 from datetime import datetime
 from enum import Enum
 from ibm_cloud_sdk_core import BaseService
+from ibm_cloud_sdk_core import DetailedResponse
 from ibm_cloud_sdk_core import datetime_to_string, string_to_datetime
-from ibm_cloud_sdk_core import get_authenticator_from_environment
+from ibm_cloud_sdk_core.get_authenticator import get_authenticator_from_environment
 from os.path import basename
 from typing import BinaryIO
 from typing import Dict
