d3d47030e45a431f45e82fa65d1435da0feab1ba
ehdsouza <erika27desouza@gmail.com>
2020-02-12 15:19:52 -0500

    chore(copyrights): correct copyrights by running linter

--- ibm_watson/speech_to_text_v1.py
+++ ibm_watson/speech_to_text_v1.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# (C) Copyright IBM Corp. 2020.
+# (C) Copyright IBM Corp. 2015, 2020.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
