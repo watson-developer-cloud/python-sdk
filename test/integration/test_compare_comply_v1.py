9024b356daf07cdcb729fc3c37c8af44411df62e
ehdsouza <erika27desouza@gmail.com>
2020-02-12 15:12:57 -0500

    test(all): run yapf on all integration tests

--- test/integration/test_compare_comply_v1.py
+++ test/integration/test_compare_comply_v1.py
@@ -7,20 +7,17 @@ from unittest import TestCase
 from ibm_watson.compare_comply_v1 import TableReturn
 
 
-@pytest.mark.skipif(
-    os.getenv('VCAP_SERVICES') is None, reason='requires VCAP_SERVICES')
+@pytest.mark.skipif(os.getenv('VCAP_SERVICES') is None,
+                    reason='requires VCAP_SERVICES')
 class IntegrationTestCompareComplyV1(TestCase):
     compare_comply = None
 
     @classmethod
     def setup_class(cls):
-        cls.compare_comply = ibm_watson.CompareComplyV1(
-            '2018-10-15')
+        cls.compare_comply = ibm_watson.CompareComplyV1('2018-10-15')
         cls.compare_comply.set_default_headers({
-            'X-Watson-Learning-Opt-Out':
-            '1',
-            'X-Watson-Test':
-            '1'
+            'X-Watson-Learning-Opt-Out': '1',
+            'X-Watson-Test': '1'
         })
 
     def test_convert_to_html(self):
@@ -32,7 +29,8 @@ class IntegrationTestCompareComplyV1(TestCase):
     def test_classify_elements(self):
         contract = abspath('resources/contract_A.pdf')
         with open(contract, 'rb') as file:
-            result = self.compare_comply.classify_elements(file, file_content_type='application/pdf').get_result()
+            result = self.compare_comply.classify_elements(
+                file, file_content_type='application/pdf').get_result()
         assert result is not None
 
     def test_extract_tables(self):
@@ -45,102 +43,97 @@ class IntegrationTestCompareComplyV1(TestCase):
     def test_compare_documents(self):
         with open(os.path.join(os.path.dirname(__file__), '../../resources/contract_A.pdf'), 'rb') as file1, \
           open(os.path.join(os.path.dirname(__file__), '../../resources/contract_B.pdf'), 'rb') as file2:
-            result = self.compare_comply.compare_documents(file1, file2).get_result()
+            result = self.compare_comply.compare_documents(file1,
+                                                           file2).get_result()
 
         assert result is not None
 
     @pytest.mark.skip(reason="Temporarily skip")
     def test_feedback(self):
         feedback_data = {
-            'feedback_type': 'element_classification',
+            'feedback_type':
+                'element_classification',
             'document': {
                 'hash': '',
                 'title': 'doc title'
             },
-            'model_id': 'contracts',
-            'model_version': '11.00',
+            'model_id':
+                'contracts',
+            'model_version':
+                '11.00',
             'location': {
                 'begin': '214',
                 'end': '237'
             },
-            'text': '1. IBM will provide a Senior Managing Consultant / expert resource, for up to 80 hours, to assist Florida Power & Light (FPL) with the creation of an IT infrastructure unit cost model for existing infrastructure.',
+            'text':
+                '1. IBM will provide a Senior Managing Consultant / expert resource, for up to 80 hours, to assist Florida Power & Light (FPL) with the creation of an IT infrastructure unit cost model for existing infrastructure.',
             'original_labels': {
-                'types': [
-                    {
-                        'label': {
-                            'nature': 'Obligation',
-                            'party': 'IBM'
-                        },
-                        'provenance_ids': [
-                            '85f5981a-ba91-44f5-9efa-0bd22e64b7bc',
-                            'ce0480a1-5ef1-4c3e-9861-3743b5610795'
-                        ]
+                'types': [{
+                    'label': {
+                        'nature': 'Obligation',
+                        'party': 'IBM'
                     },
-                    {
-                        'label': {
-                            'nature': 'End User',
-                            'party': 'Exclusion'
-                        },
-                        'provenance_ids': [
-                            '85f5981a-ba91-44f5-9efa-0bd22e64b7bc',
-                            'ce0480a1-5ef1-4c3e-9861-3743b5610795'
-                        ]
-                    }
-                ],
-                'categories': [
-                    {
-                        'label': 'Responsibilities',
-                        'provenance_ids': []
+                    'provenance_ids': [
+                        '85f5981a-ba91-44f5-9efa-0bd22e64b7bc',
+                        'ce0480a1-5ef1-4c3e-9861-3743b5610795'
+                    ]
+                }, {
+                    'label': {
+                        'nature': 'End User',
+                        'party': 'Exclusion'
                     },
-                    {
-                        'label': 'Amendments',
-                        'provenance_ids': []
-                    }
-                ]
+                    'provenance_ids': [
+                        '85f5981a-ba91-44f5-9efa-0bd22e64b7bc',
+                        'ce0480a1-5ef1-4c3e-9861-3743b5610795'
+                    ]
+                }],
+                'categories': [{
+                    'label': 'Responsibilities',
+                    'provenance_ids': []
+                }, {
+                    'label': 'Amendments',
+                    'provenance_ids': []
+                }]
             },
             'updated_labels': {
-                'types': [
-                    {
-                        'label': {
-                            'nature': 'Obligation',
-                            'party': 'IBM'
-                        }
-                    },
-                    {
-                        'label': {
-                            'nature': 'Disclaimer',
-                            'party': 'Buyer'
-                        }
+                'types': [{
+                    'label': {
+                        'nature': 'Obligation',
+                        'party': 'IBM'
                     }
-                ],
-                'categories': [
-                    {
-                        'label': 'Responsibilities'
-                    },
-                    {
-                        'label': 'Audits'
+                }, {
+                    'label': {
+                        'nature': 'Disclaimer',
+                        'party': 'Buyer'
                     }
-                ]
+                }],
+                'categories': [{
+                    'label': 'Responsibilities'
+                }, {
+                    'label': 'Audits'
+                }]
             }
         }
 
         add_feedback = self.compare_comply.add_feedback(
-            feedback_data,
-            user_id='wonder woman',
+            feedback_data, user_id='wonder woman',
             comment='test commment').get_result()
         assert add_feedback is not None
         assert add_feedback['feedback_id'] is not None
         feedback_id = add_feedback['feedback_id']
 
-        self.compare_comply.set_default_headers({'x-watson-metadata': 'customer_id=sdk-test-customer-id'})
-        get_feedback = self.compare_comply.get_feedback(feedback_id).get_result()
+        self.compare_comply.set_default_headers(
+            {'x-watson-metadata': 'customer_id=sdk-test-customer-id'})
+        get_feedback = self.compare_comply.get_feedback(
+            feedback_id).get_result()
         assert get_feedback is not None
 
         list_feedback = self.compare_comply.list_feedback(
             feedback_type='element_classification').get_result()
         assert list_feedback is not None
 
-        delete_feedback = self.compare_comply.delete_feedback(feedback_id).get_result()
+        delete_feedback = self.compare_comply.delete_feedback(
+            feedback_id).get_result()
         assert delete_feedback is not None
 
     @pytest.mark.skip(reason="Temporarily skip")
@@ -151,12 +144,9 @@ class IntegrationTestCompareComplyV1(TestCase):
         with open(os.path.join(os.path.dirname(__file__), '../../resources/cloud-object-storage-credentials-input.json'), 'rb') as input_credentials_file, \
           open(os.path.join(os.path.dirname(__file__), '../../resources/cloud-object-storage-credentials-output.json'), 'rb') as output_credentials_file:
             create_batch = self.compare_comply.create_batch(
-                'html_conversion',
-                input_credentials_file,
-                'us-south',
+                'html_conversion', input_credentials_file, 'us-south',
                 'compare-comply-integration-test-bucket-input',
-                output_credentials_file,
-                'us-south',
+                output_credentials_file, 'us-south',
                 'compare-comply-integration-test-bucket-output').get_result()
 
         assert create_batch is not None
--- test/integration/test_visual_recognition_v4.py
+++ test/integration/test_visual_recognition_v4.py
@@ -6,8 +6,9 @@ import json
 from unittest import TestCase
 from ibm_watson.visual_recognition_v4 import AnalyzeEnums, FileWithMetadata, TrainingDataObject, Location
 
-@pytest.mark.skipif(
-    os.getenv('VCAP_SERVICES') is None, reason='requires VCAP_SERVICES')
+
+@pytest.mark.skipif(os.getenv('VCAP_SERVICES') is None,
+                    reason='requires VCAP_SERVICES')
 class IntegrationTestVisualRecognitionV3(TestCase):
     visual_recognition = None
 
@@ -15,21 +16,18 @@ class IntegrationTestVisualRecognitionV3(TestCase):
     def setup_class(cls):
         cls.visual_recognition = ibm_watson.VisualRecognitionV4('2019-02-11')
         cls.visual_recognition.set_default_headers({
-            'X-Watson-Learning-Opt-Out':
-            '1',
-            'X-Watson-Test':
-            '1'
+            'X-Watson-Learning-Opt-Out': '1',
+            'X-Watson-Test': '1'
         })
 
     def test_01_colllections(self):
         collection = self.visual_recognition.create_collection(
-            name='my_collection',
-            description='just for fun'
-        ).get_result()
+            name='my_collection', description='just for fun').get_result()
         collection_id = collection.get('collection_id')
         assert collection_id is not None
 
-        my_collection = self.visual_recognition.get_collection(collection_id=collection.get('collection_id')).get_result()
+        my_collection = self.visual_recognition.get_collection(
+            collection_id=collection.get('collection_id')).get_result()
         assert my_collection is not None
         assert my_collection.get('name') == 'my_collection'
 
@@ -38,43 +36,50 @@ class IntegrationTestVisualRecognitionV3(TestCase):
             description='new description').get_result()
         assert updated_collection is not None
 
-        collections = self.visual_recognition.list_collections().get_result().get('collections')
+        collections = self.visual_recognition.list_collections().get_result(
+        ).get('collections')
         assert collections is not None
 
         self.visual_recognition.delete_collection(collection_id=collection_id)
 
     def test_02_images(self):
         collection = self.visual_recognition.create_collection(
-            name='my_collection',
-            description='just for fun'
-        ).get_result()
+            name='my_collection', description='just for fun').get_result()
         collection_id = collection.get('collection_id')
 
         add_images = self.visual_recognition.add_images(
             collection_id,
-            image_url=["https://upload.wikimedia.org/wikipedia/commons/3/33/KokoniPurebredDogsGreeceGreekCreamWhiteAdult.jpg", "https://upload.wikimedia.org/wikipedia/commons/0/07/K%C3%B6nigspudel_Apricot.JPG"],
+            image_url=[
+                "https://upload.wikimedia.org/wikipedia/commons/3/33/KokoniPurebredDogsGreeceGreekCreamWhiteAdult.jpg",
+                "https://upload.wikimedia.org/wikipedia/commons/0/07/K%C3%B6nigspudel_Apricot.JPG"
+            ],
         ).get_result()
         assert add_images is not None
         image_id = add_images.get('images')[0].get('image_id')
 
-        list_images = self.visual_recognition.list_images(collection_id).get_result()
+        list_images = self.visual_recognition.list_images(
+            collection_id).get_result()
         assert list_images is not None
 
-        image_details = self.visual_recognition.get_image_details(collection_id, image_id).get_result()
+        image_details = self.visual_recognition.get_image_details(
+            collection_id, image_id).get_result()
         assert image_details is not None
 
-        response = self.visual_recognition.get_jpeg_image(collection_id, image_id).get_result()
+        response = self.visual_recognition.get_jpeg_image(
+            collection_id, image_id).get_result()
         assert response.content is not None
 
         self.visual_recognition.delete_image(collection_id, image_id)
         self.visual_recognition.delete_collection(collection_id)
 
     def test_03_analyze(self):
-        dog_path = os.path.join(os.path.dirname(__file__), '../../resources/dog.jpg')
+        dog_path = os.path.join(os.path.dirname(__file__),
+                                '../../resources/dog.jpg')
         giraffe_path = os.path.join(os.path.dirname(__file__),
                                     '../../resources/my-giraffe.jpeg')
 
-        with open(dog_path, 'rb') as dog_file, open(giraffe_path, 'rb') as giraffe_files:
+        with open(dog_path, 'rb') as dog_file, open(giraffe_path,
+                                                    'rb') as giraffe_files:
             analyze_images = self.visual_recognition.analyze(
                 collection_ids=['684777e5-1f2d-40e3-987f-72d36557ef46'],
                 features=[AnalyzeEnums.Features.OBJECTS.value],
@@ -82,7 +87,9 @@ class IntegrationTestVisualRecognitionV3(TestCase):
                     FileWithMetadata(dog_file),
                     FileWithMetadata(giraffe_files)
                 ],
-                image_url=['https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/American_Eskimo_Dog.jpg/1280px-American_Eskimo_Dog.jpg']).get_result()
+                image_url=[
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/American_Eskimo_Dog.jpg/1280px-American_Eskimo_Dog.jpg'
+                ]).get_result()
             assert analyze_images is not None
         print(json.dumps(analyze_images, indent=2))
 
@@ -90,13 +97,16 @@ class IntegrationTestVisualRecognitionV3(TestCase):
         # create a classifier
         my_collection = self.visual_recognition.create_collection(
             name='my_test_collection',
-            description='testing for python'
-        ).get_result()
+            description='testing for python').get_result()
         collection_id = my_collection.get('collection_id')
         assert collection_id is not None
 
         # add images
-        with open(os.path.join(os.path.dirname(__file__), '../../resources/South_Africa_Luca_Galuzzi_2004.jpeg'), 'rb') as giraffe_info:
+        with open(
+                os.path.join(
+                    os.path.dirname(__file__),
+                    '../../resources/South_Africa_Luca_Galuzzi_2004.jpeg'),
+                'rb') as giraffe_info:
             add_images_result = self.visual_recognition.add_images(
                 collection_id,
                 images_file=[FileWithMetadata(giraffe_info)],
@@ -116,7 +126,8 @@ class IntegrationTestVisualRecognitionV3(TestCase):
         assert training_data is not None
 
         # list objects metadata
-        object_metadata_list = self.visual_recognition.list_object_metadata(collection_id=collection_id).get_result()
+        object_metadata_list = self.visual_recognition.list_object_metadata(
+            collection_id=collection_id).get_result()
         assert object_metadata_list is not None
 
         # update object metadata
@@ -124,8 +135,7 @@ class IntegrationTestVisualRecognitionV3(TestCase):
         updated_object_metadata = self.visual_recognition.update_object_metadata(
             collection_id=collection_id,
             object=object_metadata.get('object'),
-            new_object='updated giraffe training data'
-        ).get_result()
+            new_object='updated giraffe training data').get_result()
         assert updated_object_metadata is not None
 
         # get object metadata
@@ -142,11 +152,13 @@ class IntegrationTestVisualRecognitionV3(TestCase):
         assert train_result.get('training_status') is not None
 
         # training usage
-        training_usage = self.visual_recognition.get_training_usage(start_time='2019-11-01', end_time='2019-11-27').get_result()
+        training_usage = self.visual_recognition.get_training_usage(
+            start_time='2019-11-01', end_time='2019-11-27').get_result()
         assert training_usage is not None
 
         # delete object
-        self.visual_recognition.delete_object(collection_id, object='updated giraffe training data')
+        self.visual_recognition.delete_object(
+            collection_id, object='updated giraffe training data')
 
         # delete collection
         self.visual_recognition.delete_collection(collection_id)
