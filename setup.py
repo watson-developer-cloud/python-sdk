76a4a2a8c5666c0a84d6ccb14f8051b6b69c7f22
Angelo Paparazzi <angelo.paparazzi@ibm.com>
2024-03-13 14:14:48 -0500

    build(version): fix versioning

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 from setuptools import setup
 from os import path
 
-__version__ = '7.0.1'
+__version__ = '8.0.1'
 
 # read contents of README file
 this_directory = path.abspath(path.dirname(__file__))
